aeb85390c433e936065f68d620467f78a0eb0c91
Justin <justinrcharo@gmail.com>
2022-08-23 01:42:04 -0500

    added modals to cards

--- frontend-poke-binder/src/components/ImageCard.js
+++ frontend-poke-binder/src/components/ImageCard.js
@@ -1,9 +1,51 @@
-import React from "react";
+// import React from "react";
+// import { Button } from "semantic-ui-react";
+// import "./ImageCard.scss";
+// class ImageCard extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { spans: 0 };
+//     this.imageRef = React.createRef();
+//   }
+//   componentDidMount() {
+//     this.imageRef.current.addEventListener("load", this.setSpans);
+//   }
+//   setSpans = () => {
+//     const spans = Math.ceil(150 / 10);
+
+//     this.setState({ spans });
+//   };
+
+//   render() {
+//     const urls = this.props.image;
+//     const description = this.props.description;
+
+//     return (
+//       <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+//         <Button style={{ background: "transparent" }}>
+//           <a className="card">
+//             <img
+//               className="side front"
+//               ref={this.imageRef}
+//               alt={description}
+//               src={urls}
+//             />
+//           </a>
+//         </Button>
+//       </div>
+//     );
+//   }
+// }
+// export default ImageCard;
+import React, { useState } from "react";
+import { Header, Image, Modal } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./ImageCard.scss";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { spans: 0 };
+    this.state = { spans: 0, open: false };
     this.imageRef = React.createRef();
   }
   componentDidMount() {
@@ -18,19 +60,59 @@ class ImageCard extends React.Component {
   render() {
     const urls = this.props.image;
     const description = this.props.description;
+    const name = this.props.name;
+    const number = this.props.pokemon_number;
+    const set = this.props.pokemon_set;
 
     return (
-      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-        <a className="card">
-          <img
-            className="side front"
-            ref={this.imageRef}
-            alt={description}
-            src={urls}
-            // onClick={}
+      <Modal
+        onClose={() => this.setState({ open: false })}
+        onOpen={() => this.setState({ open: true })}
+        open={this.open}
+        trigger={
+          <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+            <Button style={{ background: "transparent" }}>
+              <a className="card">
+                <img
+                  className="side front"
+                  ref={this.imageRef}
+                  alt={description}
+                  src={urls}
+                />
+              </a>
+            </Button>
+          </div>
+        }
+      >
+        <Modal.Header>Pokemon Card</Modal.Header>
+        <Modal.Content image>
+          <Image size="medium" src={urls} wrapped centered />
+          <Modal.Description>
+            <Header>
+              {name} #{number}
+            </Header>
+            <p>{description}</p>
+            <p>Set: {set}</p>(
+            <p>
+              Would you like to add <b>{name}</b> to your Binder?
+            </p>
+            )
+          </Modal.Description>
+        </Modal.Content>
+
+        <Modal.Actions>
+          <Button color="black" onClick={() => this.setState({ open: false })}>
+            Nope
+          </Button>
+          <Button
+            content="Add Card"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={() => this.setState({ open: false })}
+            positive
           />
-        </a>
-      </div>
+        </Modal.Actions>
+      </Modal>
     );
   }
 }
--- frontend-poke-binder/src/components/ImageList.js
+++ frontend-poke-binder/src/components/ImageList.js
@@ -1,22 +1,26 @@
 import "./ImageList.css";
-import React from "react";
+
 import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const images = Object.keys(props.pokemon).map((pokemon_name) => {
     let pokemon_data = props.pokemon[pokemon_name];
-
+    let pokemon_number = pokemon_data["number"];
+    let pokemon_set = pokemon_data["set"]["name"];
     let pokemon_image = pokemon_data["images"]["small"];
 
     return (
       <ImageCard
         key={pokemon_name}
+        name={pokemon_name}
         image={pokemon_image}
+        pokemon_number={pokemon_number}
+        pokemon_set={pokemon_set}
         description={pokemon_data["flavorText"]}
       />
     );
   });
-  console.log(props.pokemon);
+
   return <div className="image-list cards-container">{images}</div>;
 };
 
